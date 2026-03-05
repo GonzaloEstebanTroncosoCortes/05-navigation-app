142e5405356bcddee3668f8df3d5def9b96ae379
Gonzalo Esteban Troncoso Cortes <gonz.troncoso@duocuc.cl>
2026-03-05 14:58:36 -0300

    backup antes de actualizar nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link, Redirect } from 'expo-router'
 
 const App = () => {
-  return (
-    <View className="mt-10">
-      <Text className=" text-3xl font-bold text-blue-500">Hola Mundo</Text>
-    </View>
-  )
+  return <Redirect href='/products'/>
+/*  return (
+ 
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className=" text-5xl" style={{fontFamily:'WorkSans-Black'}}>
+          Hola Mundo
+          </Text>
+
+        <Text className=" text-4xl text-primary font-work-black">Hola Mundo</Text>
+
+        <Text className=" text-3xl text-secondary font-work-medium">Hola Mundo</Text>
+
+        <Text className=" text-2xl text-secondary-100 font-work-light">Hola Mundo</Text>
+        <Text className=" text-xl text-tertiary">Hola Mundo</Text>
+        
+        <Link href='/products'>Productos</Link>
+
+      </View>
+    </SafeAreaView>
+  )*/
 }
 
 export default App
